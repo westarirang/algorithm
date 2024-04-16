65ddaa98b2fc381659374b94822f5a7f00c6be9a
westarirang <160497184+westarirang@users.noreply.github.com>
2024-04-16 23:18:39 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] dots) {
+		int answer = 0;
+
+	    int x1 = dots[0][0];
+	    int y1 = dots[0][1];
+	        
+	    int x2 = dots[1][0];
+	    int y2 = dots[1][1];
+	        
+	    int x3 = dots[2][0];
+	    int y3 = dots[2][1];
+	        
+	    int x4 = dots[3][0];
+	    int y4 = dots[3][1];
+	        
+	    double slope1 = (double) (y3-y1) / (x3-x1);
+	    double slope2 = (double) (y4-y2) / (x4- x2);
+	        
+	    if (slope1==slope2)
+	        return 1;
+	        
+	    double slope3 = (double) (y2-y1) / (x2-x1);
+	    double slope4 = (double) (y4-y3) / (x4-x3);
+	        
+	    if (slope3==slope4)
+	        return 1;
+	        
+	    return 0;
+    }
+}
